cfcefca3aa5301e950a0cdfb159d1788dd285d32
Adrianna Deeb <ald453@drexel.edu>
2022-12-05 22:09:49 -0500

    upload 3 -- more ui updates

--- python/final.py
+++ python/final.py
@@ -1,6 +1,5 @@
 # Main Script for Final
 
-
 # import statements
 import json
 import os
@@ -38,47 +37,39 @@ class MyMayaWidget(QWidget):
         self.button1 = QPushButton('Create Simple Rig', self)
         layout.addWidget(self.button1)
 
-        self.curve_name = QLineEdit('Curve Name', self)
-        # When the button is clicked, connect a signal to run the proper function 
+        # When button1 is clicked, connect a signal to run the proper function 
         self.button1.clicked.connect(self.button1_onClicked)   
 
         # Create button2 and add to the layout
         self.button2 = QPushButton('Reference Model Version', self)
         layout.addWidget(self.button2)
 
-        # Create text editors and add to the layout
-        self.artist_Name = QLineEdit('Artist Name', self)
-        self.version_Number = QLineEdit('Version Number', self)
-
-        # Create drop box and add to the layout
-        self.file_Ext = QComboBox()
-        self.file_Ext.addItems(['fbx','obj','mb','ma'])
-
-
         # When the button is clicked, connect a signal to run the proper function
         self.button2.clicked.connect(self.button2_onClicked)  
 
+        # Create text editors and add to the layout
+        self.artistName = QLineEdit('Artist Name', self)
+        self.versionNumber = QLineEdit('Version Number', self)
+        layout.addWidget(self.artistName)
+        layout.addWidget(self.versionNumber)
+
+        # Create drop box and add to the layout
+        self.fileExt = QComboBox()
+        self.fileExt.addItems(['fbx','obj','mb','ma'])
+     
     # create a function to define what button1 does when clicked
     def button1_onClicked(self):
-        '''
-        INSERT CODE HERE!!!!!! (i think thats all this one needs?)
-        '''
         modelPath = maya.cmds.ls(sl=True,l=True)
         rig.createJoints(modelPath)
 
-
-
     # create a function to define what button2 does when clicked
     def button2_onClicked(self):
-        '''
-        INSERT CODE HERE!!!!!! (idk where to even start this feels definitely wrong)
-        '''
         asset_info = {"project": os.getenv('project'),
                       "asset": os.getenv('asset'),
                       "task": os.getenv('task'),
-                      "artist": self.artist_Name.toPlainText(),  # usually built-in
-                      "version": self.version_NumbertoPlainText(),
-                      "ext": self.file_Ext.currentText()
+                      "artist": self.artistName.toPlainText(),  # usually built-in
+                      "version": self.versionNumber.toPlainText(),
+                      "ext": self.fileExt.currentText()
                       }
 
         naming = rig.getNamingFile()['namingConvention']
@@ -89,10 +80,6 @@ class MyMayaWidget(QWidget):
         if len(files) == 0:
             maya.cmds.warning("No file found")
         maya.cmds.file(os.path.dirname(filePath)+fileName, i=True)
-        # ^^ note: in the documentation I couldnt find anything about creating a reference in Maya?? just editing a pre-existing one??
-
-
-
 
 # initialize button and make it visible
 my_widget = MyMayaWidget()     
